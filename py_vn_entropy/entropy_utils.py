eb8509af41c28148ac7fab7c24f01e56201a958a
Longfei Fan <physflf@gmail.com>
2017-05-09 04:23:48 -0500

    entropy fixed photon number done

---
+++ py_vn_entropy/entropy_utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from qutip import *
+import laser
+
+def entropy_vs_ratio(ratios, t_list, g, kappa, nbar, N_max, init_psi):
+    """ simulate lasers with different A/C ratios
+    """
+    def get_para(ratio, nbar, kappa, g):
+        """ calculate parameters given on ratio, nbar, kappa, and g
+        """
+        gamma = np.sqrt(nbar / (alpha - 1)) * 2 * g
+        ra = 2 * kappa * nbar * alpha / (alpha - 1)
+        return {'g': g, 'gamma': gamma, 'C': kappa, 'ra': ra,
+                'A': 2 * ra * g**2 / gamma**2, 'B': 8 * ra * g**4 / gamma**4}
+    
+    step = round(len(t_list) / 100)
+    n_dict = {'gt': t_list[::step] * g}
+    entr_dict = {'gt': t_list[::step] * g}
+    l_dict = {}
+
+    for alpha in ratios:
+        paras = get_para(alpha, nbar, kappa, g)
+        g, ra, gamma, kappa = paras['g'], paras['ra'], paras['gamma'], paras['C']
+        print('ratio: {:>5.2f}, ra: {:3.4f}, A: {:.3e}, C: {:.3e}, B: {:.3e}\n'. \
+              format(alpha, ra, paras['A'], kappa, paras['B']))
+        l = laser.LaserOneMode(g, ra, gamma, kappa)
+        l.pn_evolve(init_psi, N_max, t_list)
+        
+        key = '{:.2f}'.format(alpha)
+        l_dict[key] = l
+        n_dict[key] = l.get_ns()
+        entr_dict[key] = l.get_entrs()
+
+    return l_dict, n_dict, entr_dict
+
+
+def df_plot(df, xlim, ylim, xlabel, ylabel, entropy=False):
+    """ df plot
+    """
+    df.plot(x='gt', xlim=xlim, ylim=ylim, 
+             figsize=(8, 6), fontsize=14)
+    plt.xlabel(xlabel, fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    if entropy:
+        plt.axhline(y=entropy, color='black', \
+                    linestyle='-.', label='coherent')
+    plt.legend(fontsize=14, loc=4)
